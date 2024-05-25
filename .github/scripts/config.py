ea97328bba9f1aa4e0d0874b54d08343c8d2ee81
Alexander Sabanov <alexsab@mail.ru>
2024-05-25 12:11:52 +0400

    Update config.py

--- .github/scripts/config.py
+++ .github/scripts/config.py
@@ -1,103 +1,72 @@
 dealer = {
-    "city": "Оренбург",
-    "where": "Оренбурге",
+    "city": "Самара",
+    "where": "Самаре",
 }
 # Словарь соответствия цветов
 model_mapping = {
-    "Atlas Pro": {
-        "folder": "Atlas Pro",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Черный/черный": "black-metallic.png",
-            "Серый": "gray-metallic.png",
-            "Красный": "red-metallic.png",
-            "Серебристый": "silver-metallic.png",
-        }},
-    "Atlas, I": {
-        "folder": "Atlas Pro",
-        "color": {
-
-        }},
-    "Atlas, II": {
-        "folder": "Atlas-2024",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Черный/черный": "black-metallic.png",
-            "Серебристый": "silver-metallic.png",
-            "Серый": "starry-blue-metallic.png",
-            "Белый": "white.png",
-        }},
-    "Coolray, I Рестайлинг": {
-        "folder": "New Coolray",
-        "color": {
-            "Красный": "bright-vermilion.png",
-            "Белый": "crystal-white.png",
-            "Синий": "cyber-blue.png",
-            "Серый": "magnetic-grey.png",
-            "Серебристый": "unicorn-grey.png",
-        }},
-    "Coolray, I": {
-        "folder": "Coolray",
-        "color": {
-            "Синий": "blue-metallic.png",
-            "Серый": "grey.png",
-            "Красный": "red.png",
-            "Серебристый": "silver-metallic.png",
-            "Белый": "white.png",
-        }},
-    "Emgrand L": {
-        "folder": "Emgrand",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Синий": "blue-metallic.png",
-            "Золотой": "gold-metallic.png",
-            "Серый": "gray-metallic.png",
-            "Белый": "white-metallic.png",
-        }},
-    "Emgrand, II": {
-        "folder": "Emgrand",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Синий": "blue-metallic.png",
-            "Золотой": "gold-metallic.png",
-            "Оранжевый": "gold-metallic.png",
-            "Серый": "gray-metallic.png",
-            "Белый": "white-metallic.png",
-        }},
-    "Monjaro": {
-        "folder": "Monjaro",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Черный/черный": "black-metallic.png",
-            "Зеленый": "emerald-metallic.png",
-            "Синий": "emerald-metallic.png",
-            "Серый": "gray-metallic.png",
-            "Серебристый": "silver-metallic.png",
-            "Белый": "white-metallic.png",
-        }},
-    "Okavango": {
-        "folder": "Okavango",
-        "color": {
-
-        }},
-    "Tugella, I Рестайлинг": {
-        "folder": "Tugella",
-        "color": {
-            "Черный": "black-metallic.png",
-            "Черный/черный": "black-metallic.png",
-            "Серо-голубой": "gray-blue-metallic.png",
-            "Серый": "gray-metallic.png",
-            "Белый": "white-metallic.png",
-        }},
-    "X50": {
-        "folder": "Belgee X50",
+    "E5": {
+        "folder": "e5",
         "color": {
             "Черный": "black.png",
-            "Черный/черный": "black.png",
             "Серый": "gray.png",
             "Красный": "red.png",
+            "Серебристый": "white.png",
+            "Серебряный": "white.png",
+            "Белый": "white.png",
+        }},
+    "X3": {
+        "folder": "x3",
+        "color": {
+            "black": "black.png",
+            "blue": "blue.png",
+            "dark-gray": "dark-gray.png",
+            "light-gray": "light-gray.png",
+            "red": "red.png",
+            "white": "white.png",
+            "Черный": "black.png",
             "Синий": "blue.png",
-            "Серебристый": "silver.png",
+            "Серый": "dark-gray.png",
+            "Темно-серый": "dark-gray.png",
+            "Светло-серый": "light-gray.png",
+            "Красный": "red.png",
+            "Серебристый": "white.png",
+            "Белый": "white.png",
+        }},
+    "X3 PRO": {
+        "folder": "x3pro",
+        "color": {
+            "black": "black.png",
+            "blue": "blue.png",
+            "dark-gray": "dark-gray.png",
+            "light-gray": "light-gray.png",
+            "red": "red.png",
+            "white": "white.png",
+            "Черный": "black.png",
+            "Синий": "blue.png",
+            "Серый": "dark-gray.png",
+            "Темно-серый": "dark-gray.png",
+            "Светло-серый": "light-gray.png",
+            "Красный": "red.png",
+            "Серебристый": "white.png",
+            "Белый": "white.png",
+        }},
+    "X7 Kunlun": {
+        "folder": "x7-kunlun",
+        "color": {
+            "black": "black.png",
+            "dark-blue": "dark-blue.png",
+            "dark-gray": "dark-gray.png",
+            "light-gray": "light-gray.png",
+            "purple": "purple.png",
+            "white": "white.png",
+            "Черный": "black.png",
+            "Серый": "dark-gray.png",
+            "Темно-серый": "dark-gray.png",
+            "Светло-серый": "light-gray.png",
+            "Синий": "dark-blue.png",
+            "Темно-синий": "dark-blue.png",
+            "Фиолетовый": "purple.png",
+            "Серебристый": "white.png",
             "Белый": "white.png",
         }},
     # ... добавьте другие модели по мере необходимости
